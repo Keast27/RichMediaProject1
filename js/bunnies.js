cf4b293f0777d2177c2244342ca0fc6c471bd813
Kayleigh <keast1027@gmail.com>
2021-03-03 02:00:35 -0500

    Added folder and more rough scripts

---
+++ js/bunnies.js
@@ -0,0 +1,14 @@
+class bunny {
+    constructor(x = 0, y = 0, span = 10, fwd = { x: 1, y: 0 }, speed = 0, color = "black") {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.genes = genes;
+
+       
+    }
+}
+
+function createBun(bunny){
+
+}
