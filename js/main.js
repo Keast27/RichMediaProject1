f89d74869ddffb3e5332d8416cb318aedb5da979
whitkluttz <72229679+whitkluttz@users.noreply.github.com>
2021-03-03 21:27:16 -0500

    Bunny Spawning
    Bunnies should be able to spawn in and added to an array and should also draw to the screen but aren't as of now for some reason

--- js/main.js
+++ js/main.js
@@ -2,14 +2,18 @@ import {bunny, brownBun,whiteBun,spottedBun} from './bunnies.js';
 export {init};
 // #1 - wait for page to load
 let bunbun;
-bunbun = new bunny();
+let ctx;
+bunbun = new bunny(1,2,"blue",0,0,50,1,"male",false);
+
+let bunbuns = [bunbun];
+
 function init(){
     console.log("init() called");
     // #2 - get pointer to <canvas> element on page
     let canvas = document.querySelector('canvas');
     
     // #3 - get pointer to "drawing context" and drawing API
-    let ctx = canvas.getContext('2d');
+    ctx = canvas.getContext('2d');
     
   
     ctx.fillStyle = "Skyblue"; 
@@ -21,10 +25,29 @@ function init(){
     // ctx.fillRect(x,y,width,height);
     ctx.fillRect(0,400,750,100);
     
-   bunbun.eat();
+    document.querySelector('#spawn').onclick = function()
+        {addBunBun(5,5,"white",0,0,50,1,"male",false)};
+
+    drawBunbuns();
+    bunbun.eat();
 
 }
 
+function drawBunbuns()
+{
+    console.log(bunbuns.length);
+    for(let i = 0; i < bunbuns.length; i++)
+    {
+        ctx.fillStyle = bunbuns[i].color;
+        ctx.fillRect(bunbuns[i].x, bunbuns[i].y, bunbuns[i].x + 5, bunbuns[i].y + 5);
+    }
+}
 
+function addBunBun(x,y,color,genes,hunger,health,age,sex,mated)
+{
+    let bunbunTemp = new bunny(x,y,color,genes,hunger,health,age,sex,mated);
+
+    bunbuns.push(bunbunTemp);
+}
 
 console.log("In bottom of <script> tag!");
