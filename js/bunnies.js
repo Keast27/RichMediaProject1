43ddb231b2bd9e36ff1ffe815c87493e46c3c8ce
Kayleigh <keast1027@gmail.com>
2021-03-06 22:29:29 -0500

    Bounding box added

--- js/bunnies.js
+++ js/bunnies.js
@@ -1,6 +1,6 @@
 export { bunny, brownBun, whiteBun, spottedBun };
 class bunny {
-    constructor(x = 0, y = 0, color = "brown", g1 = 1, g2 = 1, hunger = 100, health = 100, age = 0, sex = "f", mated = false, parentGenes) {
+    constructor(x = 0, y = 0, color = "brown", g1 = 1, g2 = 1, hunger = 100, health = 100, age = 0, sex = "f", mated = false) {
         //Location
         this.x = x;
         this.y = y;
@@ -16,6 +16,7 @@ class bunny {
         this.mated = mated;
         let pG = [1, 0];
         this.parentGenes = pG;
+        this.alive = true;
     }
 
     createBun() {
@@ -82,7 +83,19 @@ class bunny {
         return babyGenes;
     }
 
-    getGenes() {
+    checkCollison(bun) {
+        width = 7;
+        height = 20;
+        //get genes
+        if((bun.x < this.x + width && bun.x + width > this.x && 
+            bun.y < this.y + height && bun.y + height > this.y)){
+            if(this.sex != bun.sex){
+                if(this.sex == "f"){
+                    this.parentGenes = bun.genes;
+                    this.mated = true;
+                }
+            }
+        }
 
     }
 
@@ -98,11 +111,14 @@ class bunny {
 
         if (hunger < 70) {
             this.eat();
-        }
+        } 
+
+        if(hunger <= 0) life--;
 
         //on average, rabbits live to 9 years
         if (age == 9 || health == 0) {
-
+            this.alive = false;
+            //If dead remove from draw list
         }
 
         //Add check for pregnancy time?? idk. Pregnancy time is 4-5 weeks
